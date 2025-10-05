416b7b8d59bc6af8d3d5667950c6ee219941fa19
0xvikasrushi <0xvikas@gmail.com>
2025-10-05 22:24:23 +0530

    bug: wrong witness generator

--- wallet-unit-poc/ecdsa-spartan2/src/jwt_circuit.rs
+++ wallet-unit-poc/ecdsa-spartan2/src/jwt_circuit.rs
@@ -1,13 +1,16 @@
-use std::{env::current_dir, fs::File, io::Read, path::PathBuf};
+use std::{collections::HashMap, env::current_dir, fs::File, io::Read, path::PathBuf};
 
 use bellpepper_core::{num::AllocatedNum, ConstraintSystem, SynthesisError};
-use circom_scotia::{generate_witness_from_wasm, r1cs::CircomConfig, synthesize};
+use circom_scotia::{generate_witness_from_wasm, r1cs::CircomConfig, synthesize, witness};
+use num_traits::ToPrimitive;
+use rust_witness::{witness, BigInt};
 use spartan2::traits::circuit::SpartanCircuit;
 use tracing::info;
 
 use crate::{Scalar, E};
 
 // jwt.circom
+witness!(main);
 #[derive(Debug, Clone)]
 pub struct JWTCircuit;
 
@@ -34,16 +37,58 @@ impl SpartanCircuit<E> for JWTCircuit {
             witness_input
         };
 
-        let witness: Vec<_> = generate_witness_from_wasm(
+        info!("------------rust witness generator---------------");
+
+        let input_data: serde_json::Value = serde_json::from_str(&witness_input_json).unwrap();
+
+        // Convert JSON input to HashMap<String, Vec<Scalar>> format
+        let mut inputs_bigint: HashMap<String, Vec<BigInt>> = HashMap::new();
+
+        if let Some(obj) = input_data.as_object() {
+            for (key, value) in obj {
+                match value {
+                    serde_json::Value::String(s) => {
+                        // Parse string as BigInt first, then convert to Scalar
+                        if let Ok(bigint) = s.parse::<BigInt>() {
+                            inputs_bigint.insert(key.clone(), vec![bigint]);
+                        }
+                    }
+                    serde_json::Value::Array(arr) => {
+                        // Parse array of numbers as Vec<Scalar>
+                        let mut scalars = Vec::new();
+                        let mut bigints = Vec::new();
+                        for item in arr {
+                            if let Some(num) = item.as_u64() {
+                                scalars.push(Scalar::from(num));
+                                bigints.push(BigInt::from(num));
+                            }
+                        }
+                        inputs_bigint.insert(key.clone(), bigints);
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // Generate witness using rust-witness (replacing generate_witness_from_wasm)
+        let witness_bigints = main_witness(inputs_bigint);
+
+        // Convert Vec<BigInt> to Vec<Scalar>
+        let witness_scalars: Vec<Scalar> = witness_bigints
+            .into_iter()
+            .filter_map(|bigint| bigint.to_u64().map(Scalar::from))
+            .collect();
+
+        let witness_circom_scotia: Vec<Scalar> = generate_witness_from_wasm(
             witness_dir,
-            witness_input_json,
+            witness_input_json.clone(),
             PathBuf::from("output.wtns"),
         );
-
-        info!(witness_len = witness.len(), "witness length");
+        dbg!(&witness_scalars.len());
+        dbg!(&witness_circom_scotia.len());
 
         let cfg = CircomConfig::new(wtns, r1cs).unwrap();
-        synthesize(cs, cfg.r1cs.clone(), Some(witness))?;
+        synthesize(cs, cfg.r1cs.clone(), Some(witness_scalars))?;
         Ok(())
     }
 
--- wallet-unit-poc/ecdsa-spartan2/src/main.rs
+++ wallet-unit-poc/ecdsa-spartan2/src/main.rs
@@ -16,7 +16,11 @@ use crate::ecdsa_circuit::ECDSACircuit;
 use crate::jwt_circuit::JWTCircuit;
 use crate::setup::{run_circuit, setup_ecdsa_keys, setup_jwt_chunked_keys, setup_jwt_keys};
 
+use circom_scotia::generate_witness_from_wasm;
+use num_traits::ToPrimitive;
+use rust_witness::{witness, BigInt};
 use spartan2::{provider::T256HyraxEngine, traits::Engine};
+use std::collections::HashMap;
 use std::env::args;
 use tracing::info;
 use tracing_subscriber::EnvFilter;
@@ -28,6 +32,7 @@ mod config_generator;
 mod ecdsa_circuit;
 mod jwt_circuit;
 mod setup;
+witness!(main);
 
 fn main() {
     tracing_subscriber::fmt()
@@ -70,9 +75,91 @@ fn main() {
             info!("Spartan sumcheck + Hyrax PCS ECDSA");
             prove_ecdsa();
         }
+        "test_witness" => {
+            info!("Testing witness");
+            test_witness();
+        }
         other => {
             eprintln!("Unknown choice '{}'", other);
             std::process::exit(1);
         }
     }
+}
+
+use std::{env::current_dir, fs::File, io::Read, path::PathBuf};
+
+fn test_witness() {
+    let root = current_dir().unwrap().join("../circom");
+    let witness_dir = root.join("build/jwt/jwt_js");
+
+    let witness_input_json: String = {
+        let path = current_dir()
+            .unwrap()
+            .join("../circom/inputs/jwt/default.json");
+        let mut file = File::open(path).unwrap();
+        let mut witness_input = String::new();
+        file.read_to_string(&mut witness_input).unwrap();
+        witness_input
+    };
+
+    let witness_circom_scotia: Vec<Scalar> = generate_witness_from_wasm(
+        witness_dir,
+        witness_input_json.clone(),
+        PathBuf::from("output.wtns"),
+    );
+
+    info!(
+        "witness_circom_scotia length: {}",
+        witness_circom_scotia.len()
+    );
+
+    info!("------------rust witness generator---------------");
+
+    let input_data: serde_json::Value = serde_json::from_str(&witness_input_json).unwrap();
+
+    // Convert JSON input to HashMap<String, Vec<Scalar>> format
+    let mut inputs_bigint: HashMap<String, Vec<BigInt>> = HashMap::new();
+
+    if let Some(obj) = input_data.as_object() {
+        for (key, value) in obj {
+            match value {
+                serde_json::Value::String(s) => {
+                    // Parse string as BigInt first, then convert to Scalar
+                    if let Ok(bigint) = s.parse::<BigInt>() {
+                        inputs_bigint.insert(key.clone(), vec![bigint]);
+                    }
+                }
+                serde_json::Value::Array(arr) => {
+                    // Parse array of numbers as Vec<Scalar>
+                    let mut scalars = Vec::new();
+                    let mut bigints = Vec::new();
+                    for item in arr {
+                        if let Some(num) = item.as_u64() {
+                            scalars.push(Scalar::from(num));
+                            bigints.push(BigInt::from(num));
+                        }
+                    }
+                    inputs_bigint.insert(key.clone(), bigints);
+                }
+                _ => {}
+            }
+        }
+    }
+
+    // Generate witness using rust-witness (replacing generate_witness_from_wasm)
+    let witness_bigints = main_witness(inputs_bigint);
+
+    // Convert Vec<BigInt> to Vec<Scalar>
+    let witness_scalars: Vec<Scalar> = witness_bigints
+        .into_iter()
+        .filter_map(|bigint| bigint.to_u64().map(Scalar::from))
+        .collect();
+
+    // // assert_eq!(witness_scalars, witness_circom_scotia);
+    // for i in 0..witness_scalars.len() {
+    //     print!("index {}", i);
+    //     assert_eq!(witness_scalars[i], witness_circom_scotia[i]);
+    //     println!("");
+    // }
+    info!("witness_scalars length: {}", witness_scalars.len());
 }
